c65bc7317ecd294515676ad7e50c6e7989c20739
aksaule-bagytzhanova <aksaule124026@gmail.com>
2024-07-04 21:53:48 +0500

    cors

--- src/inside/middleware.py
+++ src/inside/middleware.py
@@ -1,6 +1,23 @@
+from django.http import HttpResponse
 from django.utils.deprecation import MiddlewareMixin
 
 class DisableCSRFMiddleware(MiddlewareMixin):
     def process_request(self, request):
         if request.path.startswith('/api/'):
             setattr(request, '_dont_enforce_csrf_checks', True)
+
+class CustomCORSMiddleware(MiddlewareMixin):
+    def process_request(self, request):
+        if request.method == "OPTIONS":
+            response = HttpResponse()
+            response['Access-Control-Allow-Origin'] = '*'
+            response['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE, OPTIONS'
+            response['Access-Control-Allow-Headers'] = 'Authorization, Content-Type'
+            return response
+        return None
+
+    def process_response(self, request, response):
+        response['Access-Control-Allow-Origin'] = '*'
+        response['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE, OPTIONS'
+        response['Access-Control-Allow-Headers'] = 'Authorization, Content-Type'
+        return response
