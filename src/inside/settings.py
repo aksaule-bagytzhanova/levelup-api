b609c7fb16828d6c7405291ee696fc56fb6ee78f
aksaule-bagytzhanova <aksaule124026@gmail.com>
2024-07-25 23:38:09 +0500

    updated CSRF_TRUSTED_ORIGINS

--- src/inside/settings.py
+++ src/inside/settings.py
@@ -263,3 +263,9 @@ REST_AUTH_SERIALIZERS = {
 
 OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
 GEMINI_API_KEY = os.getenv('GEMINI_API_KEY')
+
+CSRF_TRUSTED_ORIGINS = [
+    'https://ozger.space',
+    'https://www.ozger.space',
+    'https://back.ozger.space'
+]
